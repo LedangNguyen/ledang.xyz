bde0c8b93b84b83ff132cab314dab7905ed56b71
Le Dang Nguyen <ledangwork@gmail.com>
2023-02-06 20:40:38 +0200

    Add Contact block

---
+++ components/AppLink.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  url?: string;
+  email?: string;
+  target?: string;
+}>();
+
+const href = computed(() => {
+  if (props.email) return `mailto:${props.email}`;
+
+  return props.url;
+});
+</script>
+
+<template>
+  <a
+    class="text-lg font-medium text-white/90 transition-all duration-300 hover:text-white hover:underline hover:underline-offset-4"
+    :href="href"
+    :target="target"
+    >{{ title }}</a
+  >
+</template>
---
+++ storyblok/AppContact.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+interface Link {
+  url: string;
+  target?: string;
+}
+
+defineProps<{
+  blok: {
+    heading: string;
+    telegram: Link;
+    linkedin: Link;
+    github: Link;
+    instagram: Link;
+    resume: {
+      filename: string;
+    };
+  };
+}>();
+</script>
+
+<template>
+  <section class="pb-16 sm:pb-20" v-editable="blok">
+    <div class="container max-w-[640px]">
+      <h2 class="font-inter mb-8 text-2xl font-semibold text-white/90 sm:text-3xl">{{ blok.heading }}</h2>
+      <ul class="grid gap-4">
+        <li>
+          <AppLink :url="blok.telegram.url" :target="blok.telegram.target" title="Telegram" />
+        </li>
+        <li>
+          <AppLink :url="blok.linkedin.url" :target="blok.linkedin.target" title="Linkedin" />
+        </li>
+        <li>
+          <AppLink :url="blok.github.url" :target="blok.github.target" title="Github" />
+        </li>
+        <li>
+          <AppLink :url="blok.instagram.url" :target="blok.instagram.target" title="Instagram" />
+        </li>
+        <li>
+          <AppLink :url="blok.resume.filename" target="_blank" title="Download Resume" />
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
